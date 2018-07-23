f05e149f3126e4bee3208441438eb864f0339cd4
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-23 16:56:14 -0400

    6. Services part1 - Create HERO service to get data and inject in the HERO component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,12 +15,17 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
