575d3d9315cf59295c74d8014ea4d8536640f2f0
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-24 14:40:28 -0400

    Finished 7. Routing - HeroDetail configuration with service injection and navigation features

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,12 +8,18 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-
+  
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_fetching the heroes
     this.messageService.add('Hero Service: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id ===id));
   }
 }
